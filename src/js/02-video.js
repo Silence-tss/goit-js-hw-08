99c55b05ae2105769a25f6e24f0fd0e3b897ceab
Silence-tss <99872463+Silence-tss@users.noreply.github.com>
2023-10-02 17:57:28 +0300

    task-3

--- src/js/02-video.js
+++ src/js/02-video.js
@@ -1,16 +1,27 @@
 import Player from '@vimeo/player';
 const videoPlayer = document.querySelector("#vimeo-player");
+const player = new Vimeo.Player(videoPlayer);
+const localStorageKey = "videoplayer-current-time";
+
+    player.on('play', function() {
+        console.log('played the video!');
+    });
+
+    player.getVideoTitle().then(function(title) {
+        console.log('title:', title);
+    });
+
 
 // Отримайте поточний час відтворення відео
 const currentTime = videoPlayer.currentTime;
 
 // Збережіть поточний час відтворення в локальному сховищі
-localStorage.setItem('videoCurrentTime', currentTime);
+localStorage.setItem('localStorageKey', currentTime);
 
 // Відслідковуйте подію завантаження відео
 videoPlayer.addEventListener('loadedmetadata', () => {
   // Отримайте збережений час відтворення з локального сховища
-  const savedTime = localStorage.getItem('videoCurrentTime');
+  const savedTime = localStorage.getItem('localStorageKey');
 
   // Якщо є збережений час відтворення, встановіть його
   if (savedTime !== null) {
@@ -23,5 +34,5 @@ videoPlayer.addEventListener('loadedmetadata', () => {
 
 // Видаліть збережений час відтворення при завершенні відтворення відео
 videoPlayer.addEventListener('ended', () => {
-  localStorage.removeItem('videoCurrentTime');
+  localStorage.removeItem('localStorageKey');
 });
--- src/js/03-feedback.js
+++ src/js/03-feedback.js
@@ -1,1 +1,80 @@
 
+import throttle from 'lodash.throttle';
+const form = document.querySelector(".feedback-form");
+const emailInput = document.querySelector("input[name='email']");
+const messageTextarea = document.querySelector("textarea[name='message']");
+
+let updateTimeout;
+
+        // Відстеження події input для поля Email
+        emailInput.addEventListener("input", () => {
+            // Очищаємо попередній таймаут, якщо він існує
+            if (updateTimeout) {
+                clearTimeout(updateTimeout);
+            }
+
+            // Задаємо новий таймаут для оновлення сховища через 500 мілісекунд
+            updateTimeout = setTimeout(updateLocalStorage, 500);
+        });
+
+        // Відстеження події input для поля Message
+        messageTextarea.addEventListener("input", () => {
+            // Очищаємо попередній таймаут, якщо він існує
+            if (updateTimeout) {
+                clearTimeout(updateTimeout);
+            }
+
+            // Задаємо новий таймаут для оновлення сховища через 500 мілісекунд
+            updateTimeout = setTimeout(updateLocalStorage, 500);
+        });
+
+        // Функція для оновлення сховища
+        function updateLocalStorage() {
+            // Зчитуємо значення полів Email і Message
+            const emailValue = emailInput.value;
+            const messageValue = messageTextarea.value;
+
+            // Створюємо об'єкт для збереження значень
+            const formData = {
+                email: emailValue,
+                message: messageValue,
+            };
+
+            // Зберігаємо об'єкт у локальному сховищі
+            localStorage.setItem("feedback-form-state", JSON.stringify(formData));
+        }
+
+        // Відновлення збережених даних під час завантаження сторінки
+        const savedFormData = localStorage.getItem("feedback-form-state");
+        if (savedFormData) {
+            const parsedData = JSON.parse(savedFormData);
+            emailInput.value = parsedData.email;
+            messageTextarea.value = parsedData.message;
+        }
+
+        // Обробник події подачі форми
+        form.addEventListener("submit", (e) => {
+            e.preventDefault();
+
+            // Очищка сховища
+            localStorage.removeItem("feedback-form-state");
+
+            // Вивід об'єкта з поточними значеннями полів у консоль
+            const emailValue = emailInput.value;
+            const messageValue = messageTextarea.value;
+            const formData = {
+                email: emailValue,
+                message: messageValue,
+            };
+            console.log(formData);
+
+            // Очищка полів форми
+            emailInput.value = "";
+            messageTextarea.value = "";
+        });
+
+
+
+
+
+
